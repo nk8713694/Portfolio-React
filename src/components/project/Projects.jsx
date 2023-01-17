25b50fd022ca6eaa82d3d42e4aaa60362def225c
unknown <nk8713694@gmail.com>
2023-01-17 08:05:52 +0530

    update project

--- src/components/project/Projects.jsx
+++ src/components/project/Projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './project.css';
 import IMG3 from '../../assets/covid.jpg';
+import commentsystem from '../../assets/commentsystem.PNG';
 
 const projects = [
   {
@@ -10,34 +11,55 @@ const projects = [
     github: 'https://github.com/nk8713694/react-redux-capstone',
     demo: 'https://determinedx-aryabhata-b6bc71.netlify.app/',
   },
+  {
+    id: 4,
+    image: IMG3,
+    title: ' Similar website to https://usabilityhub.com/ ',
+    github: 'https://github.com/nk8713694/HtmlCSSProject',
+    demo: 'https://fluffy-cocada-b36abe.netlify.app/',
+  },
+  {
+    id: 5,
+    image: commentsystem,
+    title: ' Build Comment System like Any Social Media App',
+    github: 'https://github.com/nk8713694/BuildCommentSystem',
+    demo: 'https://mellow-choux-ae6c2b.netlify.app/',
+  },
 ];
 const Projects = () => {
   return (
-    <section id="project">
+    <section id='project'>
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
-      <div className="container project_container">
+      <div className='container project_container'>
         {projects.map(({ id, image, title, github, demo }) => {
           return (
-            <article key={id} className="project_item">
-              <div className="project_item-image">
-                <img src={image} alt={title} />
+            <article key={id} className='project_item'>
+              <div className='project_item-image'>
+                <img
+                  src={image}
+                  alt={title}
+                  width='100%'
+                  height='100%'
+                  object-fit='contain'
+                  background-size='cover'
+                />
               </div>
               <h3>{title}</h3>
-              <div className="project-item-cta">
+              <div className='project-item-cta'>
                 <a
                   href={github}
-                  className="btn"
-                  target="_blank"
-                  rel="noreferrer"
+                  className='btn'
+                  target='_blank'
+                  rel='noreferrer'
                 >
                   Github
                 </a>
                 <a
                   href={demo}
-                  className="btn btn-primary"
-                  target="_blank"
-                  rel="noreferrer"
+                  className='btn btn-primary'
+                  target='_blank'
+                  rel='noreferrer'
                 >
                   Live Demo
                 </a>
